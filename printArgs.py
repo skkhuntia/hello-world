5518dfc6df586afedcdac8fd47773a677db759da
Souman Khuntia <31050599+skkhuntia@users.noreply.github.com>
2017-11-30 11:56:24 -0800

    Print Arguments
    Function to Print Arguments to emulate the k8_utils.py

---
+++ printArgs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import sys
+
+def printArguments(namespace, node_ip, pod_name):
+    print("Namespace: \t" + namespace)
+    print("Node IP: \t" + node_ip)
+    print("Pod Name: \t" + pod_name)
