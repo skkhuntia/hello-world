d81946d092645ddff4277268c4b291e92ba8f0a4
Souman Khuntia <31050599+skkhuntia@users.noreply.github.com>
2017-11-30 13:57:23 -0800

    Update callFunctionFromFile.py
    Yet to Configure SMTP on Jenkins to Test Email Notification

--- callFunctionFromFile.py
+++ callFunctionFromFile.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
-##import printArgs
+import printArgs
 
 printArgs.printArguments(sys.argv[1], sys.argv[2], sys.argv[3])
+
+## Configure SMTP on Jenkins to Test Email Notification
