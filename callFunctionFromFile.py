2de73c6fdda658d04f8a8fda3f73b81117528648
Souman Khuntia <31050599+skkhuntia@users.noreply.github.com>
2017-11-30 13:52:10 -0800

    Update callFunctionFromFile.py
    Updated Correct File Name "printArgs"

--- callFunctionFromFile.py
+++ callFunctionFromFile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
 import sys
-import hello-world/basicForArgsCallFunc
+import printArgs
 
-basicForArgsCallFunc.printArguments(sys.argv[1], sys.argv[2], sys.argv[3])
+printArgs.printArguments(sys.argv[1], sys.argv[2], sys.argv[3])
