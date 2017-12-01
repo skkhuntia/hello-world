cd8de388c9ad4f698f5b36b58a09f9a11f3b8fe9
Souman Khuntia <31050599+skkhuntia@users.noreply.github.com>
2017-12-01 13:50:41 -0800

    Output Collection
    Collect output returned from calling function

--- callFunctionFromFile.py
+++ callFunctionFromFile.py
@@ -3,6 +3,9 @@
 import sys
 import printArgs
 
-printArgs.printArguments(sys.argv[1], sys.argv[2], sys.argv[3])
-
+returnVal = printArgs.printArguments(sys.argv[1], sys.argv[2], sys.argv[3])
+if returnVal==true:
+    print("Success")
+else:
+    print("False")
 ## Configure SMTP on Jenkins to Test Email Notification
