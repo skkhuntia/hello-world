fb28ae519d41c9d5d129fcc0caacb6d3dbe570a3
Souman Khuntia <31050599+skkhuntia@users.noreply.github.com>
2017-12-01 13:56:30 -0800

    Update callFunctionFromFile.py
    true --> True

--- callFunctionFromFile.py
+++ callFunctionFromFile.py
@@ -4,8 +4,8 @@ import sys
 import printArgs
 
 returnVal = printArgs.printArguments(sys.argv[1], sys.argv[2], sys.argv[3])
-if returnVal==true:
+if returnVal==True:
     print("Success")
 else:
-    print("False")
+    print("Fail")
 ## Configure SMTP on Jenkins to Test Email Notification
