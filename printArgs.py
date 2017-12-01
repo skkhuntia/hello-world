a51f2994fcce7532656ce1d905f39489c257551b
Souman Khuntia <31050599+skkhuntia@users.noreply.github.com>
2017-12-01 13:47:13 -0800

    Test Output returned
    Calling Function returns output based on parameters passed by Jenkins Job

--- printArgs.py
+++ printArgs.py
@@ -6,3 +6,6 @@ def printArguments(namespace, node_ip, pod_name):
     print("Namespace: \t" + namespace)
     print("Node IP: \t" + node_ip)
     print("Pod Name: \t" + pod_name)
+    
+    if namespace=="csp-qa":
+        return true
